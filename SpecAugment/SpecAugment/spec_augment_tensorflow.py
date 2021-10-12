fbe07e4269a3e9aeb0f32c38f697205572917ebd
HouBiHuang <r60179k09209@gmail.com>
2021-10-12 13:53:51 +0800

    update

--- SpecAugment/SpecAugment/spec_augment_tensorflow.py
+++ SpecAugment/SpecAugment/spec_augment_tensorflow.py
@@ -7,97 +7,40 @@ import matplotlib.pyplot as plt
 import random
 
 
-def sparse_warp(mel_spectrogram, time_warping_para=2):
-    """Spec augmentation Calculation Function.
-
-    'SpecAugment' have 3 steps for audio data augmentation.
-    first step is time warping using Tensorflow's image_sparse_warp function.
-    Second step is frequency masking, last step is time masking.
-
-    # Arguments:
-      mel_spectrogram(numpy array): audio file path of you want to warping and masking.
-      time_warping_para(float): Augmentation parameter, "time warp parameter W".
-        If none, default = 80 for LibriSpeech.
-
-    # Returns
-      mel_spectrogram(numpy array): warped and masked mel spectrogram.
-    """
-    
-    """
-    fbank_size = tf.shape(mel_spectrogram)
-    n, v = fbank_size[1], fbank_size[2]
-
-    # Step 1 : Time warping
-    # Image warping control point setting.
-    # Source
-    pt = tf.random.uniform([], time_warping_para, n-time_warping_para, tf.int32) # radnom point along the time axis
-    src_ctr_pt_freq = tf.range(v // 2)  # control points on freq-axis
-    src_ctr_pt_time = tf.ones_like(src_ctr_pt_freq) * pt  # control points on time-axis
-    src_ctr_pts = tf.stack((src_ctr_pt_time, src_ctr_pt_freq), -1)
-    src_ctr_pts = tf.cast(src_ctr_pts, dtype=tf.float32)
-
-    # Destination
-    w = tf.random.uniform([], -time_warping_para, time_warping_para, tf.int32)  # distance
-    dest_ctr_pt_freq = src_ctr_pt_freq
-    dest_ctr_pt_time = src_ctr_pt_time + w
-    dest_ctr_pts = tf.stack((dest_ctr_pt_time, dest_ctr_pt_freq), -1)
-    dest_ctr_pts = tf.cast(dest_ctr_pts, dtype=tf.float32)
-
-    # warp
-    source_control_point_locations = tf.expand_dims(src_ctr_pts, 0)  # (1, v//2, 2)
-    dest_control_point_locations = tf.expand_dims(dest_ctr_pts, 0)  # (1, v//2, 2)
-
-    warped_image, _ = sparse_image_warp(mel_spectrogram,
-                                        source_control_point_locations,
-                                        dest_control_point_locations)
-    return warped_image
-    """
-    
-        
+def sparse_warp(mel_spectrogram, time_warping_para=2):         
     v, tau = mel_spectrogram.shape[1], mel_spectrogram.shape[2]
         
-    horiz_line_thru_ctr = mel_spectrogram[0][v//2]
+    horiz_line_thru_ctr = mel_spectrogram[0][v//2] #取得梅爾頻譜圖中心點
     
-    random_pt = horiz_line_thru_ctr[random.randrange(time_warping_para, tau - time_warping_para)] # random point along the horizontal/time axis
-    w = np.random.uniform((-time_warping_para), time_warping_para) # distance
+    random_pt = horiz_line_thru_ctr[random.randrange(time_warping_para, tau - time_warping_para)] #沿著水平時間軸隨機取得一點
+    w = np.random.uniform((-time_warping_para), time_warping_para) #距離
         
     # Source Points
     src_points = [[[v//2, random_pt[0]]]]
         
     # Destination Points
     dest_points = [[[v//2, random_pt[0] + w]]]
-        
+
+    #How many zero-flow boundary points to include at each image edge. Usage:
+    #num_boundary_points=2: 4 corners and one in the middle of each edge (8 points total)
     mel_spectrogram, _ = sparse_image_warp(mel_spectrogram, src_points, dest_points, num_boundary_points=2)
     
     return mel_spectrogram
 
-def time_masking(mel_spectrogram, tau, time_masking_para=3, time_mask_num=1):
-    """Spec augmentation Calculation Function.
 
-    'SpecAugment' have 3 steps for audio data augmentation.
-    first step is time warping using Tensorflow's image_sparse_warp function.
-    Second step is frequency masking, last step is time masking.
-
-    # Arguments:
-      mel_spectrogram(numpy array): audio file path of you want to warping and masking.
-      frequency_masking_para(float): Augmentation parameter, "frequency mask parameter F"
-        If none, default = 100 for LibriSpeech.
-      frequency_mask_num(float): number of frequency masking lines, "m_F".
-        If none, default = 1 for LibriSpeech.
-
-    # Returns
-      mel_spectrogram(numpy array): warped and masked mel spectrogram.
-    """
+def time_masking(mel_spectrogram, time_masking_para=3, time_mask_num=1):
     # Step 2 : time masking
     fbank_size = tf.shape(mel_spectrogram)
     n, tau = fbank_size[1], fbank_size[2]
 
+    #產生一堆0&1，再跟mel_spectrogram相乘，乘0的地方代表被遮蔽
     for i in range(time_mask_num):
+        #產生遮蔽範圍
         t = tf.random.uniform([], minval=0, maxval=time_masking_para, dtype=tf.int32)
         tau = tf.cast(tau, dtype=tf.int32)
         t0 = tf.random.uniform([], minval=0, maxval=tau-t, dtype=tf.int32)
 
-        # warped_mel_spectrogram[f0:f0 + f, :] = 0
+        # warped_mel_spectrogram[:, t0:t0 + t] = 0
         mask = tf.concat((tf.ones(shape=(1, n, tau - t0 - t, 1)),
                           tf.zeros(shape=(1, n, t, 1)),
                           tf.ones(shape=(1, n, t0, 1)),
@@ -106,23 +49,7 @@ def time_masking(mel_spectrogram, tau, time_masking_para=3, time_mask_num=1):
     return tf.cast(mel_spectrogram, dtype=tf.float32)
 
 
-def frequency_masking(mel_spectrogram, v, frequency_masking_para=50, frequency_mask_num=1):
-    """Spec augmentation Calculation Function.
-
-    'SpecAugment' have 3 steps for audio data augmentation.
-    first step is time warping using Tensorflow's image_sparse_warp function.
-    Second step is frequency masking, last step is time masking.
-
-    # Arguments:
-      mel_spectrogram(numpy array): audio file path of you want to warping and masking.
-      time_masking_para(float): Augmentation parameter, "time mask parameter T"
-        If none, default = 27 for LibriSpeech.
-      time_mask_num(float): number of time masking lines, "m_T".
-        If none, default = 1 for LibriSpeech.
-
-    # Returns
-      mel_spectrogram(numpy array): warped and masked mel spectrogram.
-    """
+def frequency_masking(mel_spectrogram, frequency_masking_para=50, frequency_mask_num=1):
     fbank_size = tf.shape(mel_spectrogram)
     v, n = fbank_size[1], fbank_size[2]
 
@@ -130,8 +57,8 @@ def frequency_masking(mel_spectrogram, v, frequency_masking_para=50, frequency_m
     for i in range(frequency_mask_num):
         f = tf.random.uniform([], minval=0, maxval=frequency_masking_para, dtype=tf.int32)
         f0 = tf.random.uniform([], minval=0, maxval=v-f, dtype=tf.int32)
-
-        # mel_spectrogram[:, t0:t0 + t] = 0
+        
+        # mel_spectrogram[f0:f0 + f,:] = 0
         mask = tf.concat((tf.ones(shape=(1, v-f0-f, n, 1)),
                           tf.zeros(shape=(1, f, n, 1)),
                           tf.ones(shape=(1, f0, n, 1)),
@@ -139,6 +66,7 @@ def frequency_masking(mel_spectrogram, v, frequency_masking_para=50, frequency_m
         
         mel_spectrogram = mel_spectrogram * mask
     return tf.cast(mel_spectrogram, dtype=tf.float32)
+
 
 
 def spec_augment(mel_spectrogram):
@@ -153,10 +81,10 @@ def spec_augment(mel_spectrogram):
     warped_mel_spectrogram = sparse_warp(mel_spectrogram)
 
     #時間遮罩
-    warped_time_spectrogram = time_masking(warped_mel_spectrogram, tau=tau)
+    warped_time_spectrogram = time_masking(warped_mel_spectrogram)
 
     #頻率遮罩
-    warped_time_frequency_sepctrogram = frequency_masking(warped_time_spectrogram, v=v)
+    warped_time_frequency_sepctrogram = frequency_masking(warped_time_spectrogram)
 
     #重整shape到原本模樣
     warped_time_frequency_sepctrogram = np.reshape(warped_time_frequency_sepctrogram, (v, tau))
@@ -165,18 +93,6 @@ def spec_augment(mel_spectrogram):
 
 
 def visualization_spectrogram(mel_spectrogram, title):
-    """visualizing first one result of SpecAugment
-
-    # Arguments:
-      mel_spectrogram(ndarray): mel_spectrogram to visualize.
-      title(String): plot figure's title
-    """
-    # Show mel-spectrogram using librosa's specshow.
-    #plt.figure(figsize=(10, 4))
-    #librosa.display.specshow(librosa.power_to_db(mel_spectrogram[0, :, :, 0], ref=np.max), y_axis='mel', fmax=16000, x_axis='time')
-    #plt.title(title)
-    #plt.tight_layout()
-    #plt.show()
     fig, ax = plt.subplots()
     S_dB = librosa.power_to_db(mel_spectrogram[:, :], ref=np.max)
     img = librosa.display.specshow(S_dB, x_axis='time',
@@ -187,20 +103,6 @@ def visualization_spectrogram(mel_spectrogram, title):
 
 
 def visualization_tensor_spectrogram(mel_spectrogram, title):
-    """visualizing first one result of SpecAugment
-
-    # Arguments:
-      mel_spectrogram(ndarray): mel_spectrogram to visualize.
-      title(String): plot figure's title
-    """
-
-    # Show mel-spectrogram using librosa's specshow.
-    #plt.figure(figsize=(10, 4))
-    #librosa.display.specshow(librosa.power_to_db(mel_spectrogram[0, :, :, 0], ref=np.max), y_axis='mel', fmax=16000, x_axis='time')
-    # plt.colorbar(format='%+2.0f dB')
-    #plt.title(title)
-    #plt.tight_layout()
-    #plt.show()
     fig, ax = plt.subplots()
     S_dB = librosa.power_to_db(mel_spectrogram[:, :], ref=np.max)
     img = librosa.display.specshow(S_dB, x_axis='time',
--- SpecAugment/spec_augment_test_TF.py
+++ SpecAugment/spec_augment_test_TF.py
@@ -1,35 +1,16 @@
-import argparse
 import librosa
-from SpecAugment import spec_augment_tensorflow
-import os, sys
+#from SpecAugment import spec_augment_tensorflow
+import SpecAugment.spec_augment_tensorflow
 import numpy as np
-import tensorflow as tf
 from scipy.io import wavfile
-# sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+import importlib
+importlib.reload(SpecAugment.spec_augment_tensorflow)
 
-parser = argparse.ArgumentParser(description='Spec Augment')
-parser.add_argument('--audio-path', default='../data/61-70968-0002.wav',
-                    help='The audio file.')
-parser.add_argument('--time-warp-para', default=80,
-                    help='time warp parameter W')
-parser.add_argument('--frequency-mask-para', default=27,
-                    help='frequency mask parameter F')
-parser.add_argument('--time-mask-para', default=100,
-                    help='time mask parameter T')
-parser.add_argument('--masking-line-number', default=1,
-                    help='masking line number')
-
-args = parser.parse_args()
-time_warping_para = args.time_warp_para
-time_masking_para = args.frequency_mask_para
-frequency_masking_para = args.time_mask_para
-masking_line_number = args.masking_line_number
-
-words = ["ㄏㄧㄡ","ㄟ","他","好","你","吼","我","那","那那個","的","的一個","的那個","的這個","阿","啦","著","嗯"]
+words = ["ㄏㄧㄡ"]#,"ㄟ","他","好","你","吼","我","那","那那個","的","的一個","的那個","的這個","阿","啦","著","嗯"]
 
 if __name__ == "__main__":
     for word in words:
-        for i in range(1,41):
+        for i in range(1,2):
             audio_path = "./recordingSpecAugment/{0}/{0}{1}.wav".format(word,i) #args.audio_path
             # Step 0 : load audio file, extract mel spectrogram
             audio, sampling_rate = librosa.load(audio_path,sr=16000)
@@ -38,26 +19,19 @@ if __name__ == "__main__":
                                                              n_mels=256,
                                                              fmax=16000)
             
-            # reshape spectrogram shape to [batch_size, time, frequency, 1]
-            shape = mel_spectrogram.shape
-            #mel_spectrogram = np.reshape(mel_spectrogram, (-1, shape[0], shape[1], 1))
             # Show Raw mel-spectrogram
-            #spec_augment_tensorflow.visualization_spectrogram(mel_spectrogram=mel_spectrogram,
-            #                                                  title="Raw Mel Spectrogram")
+            SpecAugment.spec_augment_tensorflow.visualization_spectrogram(mel_spectrogram=mel_spectrogram,
+                                                              title="Raw Mel Spectrogram")
             
             for j in range(1,6):
                 
                 #做資料增強
-                warped_masked_spectrogram = spec_augment_tensorflow.spec_augment(mel_spectrogram)
+                warped_masked_spectrogram = SpecAugment.spec_augment_tensorflow.spec_augment(mel_spectrogram)
             
                 # Show time warped & masked spectrogram
-                #spec_augment_tensorflow.visualization_tensor_spectrogram(mel_spectrogram=warped_masked_spectrogram,
-                #                                                  title="tensorflow Warped & Masked Mel Spectrogram")
+                SpecAugment.spec_augment_tensorflow.visualization_tensor_spectrogram(mel_spectrogram=warped_masked_spectrogram,
+                                                                  title="tensorflow Warped & Masked Mel Spectrogram")
                 
-                #print(warped_masked_spectrogram.shape)
-                
-                #warped_masked_spectrogram = np.reshape(warped_masked_spectrogram, (shape[0], shape[1]))
-                #print(warped_masked_spectrogram)
                 
                 #Change spectrogram to audio
                 mel_to_stft = librosa.feature.inverse.mel_to_stft(warped_masked_spectrogram)
